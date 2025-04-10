642f76297b18e4be04e46145ccb12b97ff883129
Christos Savva <info@velistar.com>
2025-04-10 14:12:33 -0300

    Yielding correctly the DB

--- app/db/postgres.py
+++ app/db/postgres.py
@@ -1,3 +1,5 @@
+from typing import Any, AsyncGenerator
+
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
 import app.core.config as config
@@ -9,6 +11,6 @@ Base = declarative_base()
 
 
 # Database dependency
-async def get_db() -> AsyncSession:
+async def get_db() -> AsyncGenerator[AsyncSession, Any]:
     async with AsyncSessionLocal() as session:
         yield session
