97ada92ed9a2f1fae93108d71acfb34a0ac805f0
Christos Savva <info@velistar.com>
2025-04-10 09:05:11 -0300

    Added tags to main routes

--- app/main.py
+++ app/main.py
@@ -2,68 +2,68 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.post("/auth/register")
+@app.post("/auth/register", tags=["auth"])
 def register():
     #TODO: Implement registration logic
     pass
 
-@app.post("/auth/login")
+@app.post("/auth/login", tags=["auth"])
 def login():
     #TODO: Implement login logic
     pass
 
-@app.get("/users/me")
+@app.get("/users/me", tags=["auth"])
 def get_current_user():
     #TODO: Implement logic to get current user
     pass
 
-@app.post("/posts/")
+@app.post("/posts/", tags=["posts"])
 def create_post():
     #TODO: Implement logic to create a new post
     pass
 
-@app.get("/posts/")
+@app.get("/posts/", tags=["posts"])
 def list_posts():
     #TODO: Implement logic to list all posts
     pass
 
-@app.get("/posts/{slug}")
+@app.get("/posts/{slug}", tags=["posts"])
 def get_post(slug: str):
     #TODO: Implement logic to get a specific post
     pass
 
-@app.put("/posts/{slug}")
+@app.put("/posts/{slug}", tags=["posts"])
 def update_post(slug: str):
     #TODO: Implement logic to update a specific post
     pass
 
-@app.delete("/posts/{slug}")
+@app.delete("/posts/{slug}", tags=["posts"])
 def delete_post(slug: str):
     #TODO: Implement logic to delete a specific post
     pass
 
-@app.get("/tags/")
+@app.get("/tags/", tags=["tags"])
 def get_tags():
     #TODO: Implement logic to get all tags
     pass
 
-@app.post("/posts/{slug}/tags")
+@app.post("/posts/{slug}/tags", tags=["tags", "posts"])
 def add_post_tags(slug: str):
     #TODO: Implement logic to add tags to a specific post
     pass
 
 
-@app.get("/posts/{slug}/versions")
+@app.get("/posts/{slug}/versions", tags=["posts", "versions"])
 def get_posts_versions(slug: str):
     #TODO: Implement logic to get all versions of a specific post
     pass
 
-@app.post("/posts/{slug}/restore/{version_id}")
+@app.post("/posts/{slug}/restore/{version_id}", tags=["posts", "versions"])
 def restore_post(slug: str, version_id: int):
     #TODO: Implement logic to restore a specific version of a post
     pass
 
-@app.get("/rss.xml")
+@app.get("/rss.xml", tags=["rss"])
 def get_rss():
     #TODO: Implement logic to get RSS feed
     pass
