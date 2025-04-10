ec2694a0997614a102750a9d543cff3cb859c7e1
Christos Savva <info@velistar.com>
2025-04-10 09:01:43 -0300

    Set up routes on main

--- app/main.py
+++ app/main.py
@@ -2,12 +2,68 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.post("/auth/register")
+def register():
+    #TODO: Implement registration logic
+    pass
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/auth/login")
+def login():
+    #TODO: Implement login logic
+    pass
+
+@app.get("/users/me")
+def get_current_user():
+    #TODO: Implement logic to get current user
+    pass
+
+@app.post("/posts/")
+def create_post():
+    #TODO: Implement logic to create a new post
+    pass
+
+@app.get("/posts/")
+def list_posts():
+    #TODO: Implement logic to list all posts
+    pass
+
+@app.get("/posts/{slug}")
+def get_post(slug: str):
+    #TODO: Implement logic to get a specific post
+    pass
+
+@app.put("/posts/{slug}")
+def update_post(slug: str):
+    #TODO: Implement logic to update a specific post
+    pass
+
+@app.delete("/posts/{slug}")
+def delete_post(slug: str):
+    #TODO: Implement logic to delete a specific post
+    pass
+
+@app.get("/tags/")
+def get_tags():
+    #TODO: Implement logic to get all tags
+    pass
+
+@app.post("/posts/{slug}/tags")
+def add_post_tags(slug: str):
+    #TODO: Implement logic to add tags to a specific post
+    pass
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/posts/{slug}/versions")
+def get_posts_versions(slug: str):
+    #TODO: Implement logic to get all versions of a specific post
+    pass
+
+@app.post("/posts/{slug}/restore/{version_id}")
+def restore_post(slug: str, version_id: int):
+    #TODO: Implement logic to restore a specific version of a post
+    pass
+
+@app.get("/rss.xml")
+def get_rss():
+    #TODO: Implement logic to get RSS feed
+    pass
